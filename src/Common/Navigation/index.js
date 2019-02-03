fcad0e48d5495db8267c1e2058f9dc69be114c4e
tommy3531 <thomas.marler@hotmail.com>
2019-02-02 20:42:46 -0600

    Fixed netilfy error

--- src/Common/Navigation/index.js
+++ src/Common/Navigation/index.js
@@ -7,7 +7,6 @@ import '../../styles/main.css';
 class Navigation extends Component {
   render() {
     return (
-
       <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
       <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="#page-top">Sheep Herder</a>
